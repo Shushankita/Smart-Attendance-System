32c42ce3676d68bb78a322485177707488da30ef
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-17 12:25:00 +0530

    Update and rename 1.2 Add lable to the window.py to 1.2 Add label to the window.py

---
+++ 1.2 Add label to the window.py
@@ -0,0 +1,27 @@
+'''Task1: Change the text on the label to Smart Attendance System
+Task2: Select any other font style from lines 16 to 23. Remove all the other font styles from the code.'''
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Training Program")
+root.geometry("500x300")
+
+# Set the background colour to blue
+root.configure(bg='skyblue')
+
+
+label1 = tk.Label(root, text="Label1", font=("Courier New", 24), bg="blue", fg="white") #changing the font style and adding foreground color(fg) and background color(bg)
+"""List of font-style available
+1. Arial
+2. Times New Roman
+3. Comic Sans MS
+4. Courier New
+5. Impact
+6. Georgia
+7. Lexend (
+8. Comfortaa"""
+label1.pack()
+
+
+root.mainloop()
