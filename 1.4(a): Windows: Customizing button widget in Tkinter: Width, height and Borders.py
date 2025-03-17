77e4c25307ee9d7333f3ac92b497c707d505c014
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-17 16:27:27 +0530

    Create 1.4(a): Windows: Customizing button widget in Tkinter: Width, height and Borders.py

---
+++ 1.4(a): Windows: Customizing button widget in Tkinter: Width, height and Borders.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Training Program")
+root.geometry("500x300")
+
+root.configure(bg='skyblue')
+
+
+label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), bg="blue", fg="white") 
+label1.pack()
+
+button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8") #defining the width and height of the button in terms of pixels
+button1.config(bd = 3,relief="solid")#adding border around the button and increasing it's thickness
+button1.pack()
+
+root.mainloop()
