c6abd646f999911f432b2a7eb87fccbb58661525
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-17 12:30:20 +0530

    Update 1.3 Add button to the window.py

--- 1.3 Add button to the window.py
+++ 1.3 Add button to the window.py
@@ -1,17 +1,22 @@
+'''Observe the code and find out the reason why the button is not visible.
+Task1: Uncomment line19 to make the button visible.
+Task2: Change the text on the button to "Select Excel File" '''
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Training Program")
 root.geometry("500x300")
-
 root.configure(bg='skyblue')
 
 
 label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), bg="blue", fg="white") 
 label1.pack()
 
-button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8") #defining the width and height of the button in terms of pixels
-button1.config(highlightbackground = "black",highlightthickness=3)#adding border around the button and increasing it's thickness
-button1.pack()
+ #defining the width and height of the button in terms of pixels
+button1 = tk.Button(root, text="Button1",width=20,height=2,bg="#B4A3D8")
+
+#adding a border around the button and increasing its thickness
+button1.config(highlightbackground = "black",highlightthickness=3)
+#button1.pack()
 
 root.mainloop()
