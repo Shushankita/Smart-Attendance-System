b29fb909a71d39cfc7a3776f32529ebf30da5443
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-17 12:21:52 +0530

    Update 1.1 Basic window.py

--- 1.1 Basic window.py
+++ 1.1 Basic window.py
@@ -1,20 +1,13 @@
-"""Task1: Change the title of the code to Smart Attendance System
-Task2: Change the text on the button to 'Select the Excel sheet' """
+""" Run and observe the code.
+Task1: Change the title of the code to Smart Attendance System
+Task2: Change the geometry to 500X300 """
 
 import tkinter as tk
 
 # Create the main tkinter window
 root = tk.Tk()
 root.title("Training Program")
-root.geometry("500x300")
+root.geometry("500x500")
 
-# Create a label widget
-label1 = tk.Label(root, text="Smart Attendance System")
-label1.pack()
-
-# Create a button to select the Excel file
-button1 = tk.Button(root, text="Button1")
-button1.pack()
-
-# Run the tkinter event loop
+# Run the Tkinter event loop
 root.mainloop()
