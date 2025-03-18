c5c2756406ac12d84a265afa3d8537a65ff45db2
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-18 09:22:52 +0530

    Update 1.8 Add more labels and buttons.py

--- 1.8 Add more labels and buttons.py
+++ 1.8 Add more labels and buttons.py
@@ -1,35 +1,34 @@
+# Session 3
+# Task 1: Make the date_label visible.
+# Task 2: Add another button by name button2 with text as 'Upload Photo' similar to button2.
+# Task 3: Add a submit button with the following properties:
+# bg="black", fg="white",activebackground="blue",activeforeground="white"
+
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Training Program")
 root.geometry("500x300")
 
-root.configure(bg='skyblue')
+root.config(bg='skyblue')
 
 
-label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), 
-bg="blue", fg="white") 
+label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24),
+bg="blue", fg="white")
 label1.pack(padx=10,pady=10)
 
 #label to add text "Select Date"
 date_label = tk.Label(root, text="Select Date:",font=("Courier New", 10), bg="skyblue")
-date_label.pack(pady=5)
-
 
 button1 = tk.Button(root, text="Select Excel File",width=20,height=2,
 bg="#B4A3D8",activebackground="grey",activeforeground="white")
 button1.config(highlightbackground = "black",highlightthickness=3)
 button1.pack(pady=10)
 
-#button to upload photo
-#button2=tk.Button(root,text="Upload Photo",width=20,height=2,bg="#B4A3D8",
-#activebackground="grey",activeforeground="white")
-#button2.config(highlightbackground = "black",highlightthickness=3)
-#button2.pack(pady=10)
+#button to upload a photo
+
 
 #button to submit
-submit=tk.Button(root,text="Submit",width=20,height=2,bg="black",
-fg="white",activebackground="blue",activeforeground="white")
-submit.pack(pady=10)
+
 
 root.mainloop()
