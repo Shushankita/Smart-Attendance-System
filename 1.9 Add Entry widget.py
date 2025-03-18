c946c35e0dad76d17cd5989b7411b64c2b5240ce
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-03-18 09:30:05 +0530

    Update 1.9 Add Entry widget.py

--- 1.9 Add Entry widget.py
+++ 1.9 Add Entry widget.py
@@ -1,20 +1,25 @@
+# Session 4
+# Task: Make the entry widget visible on the screen
+
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Training Program")
 root.geometry("500x300")
 
-root.configure(bg='skyblue')
+root.config(bg='skyblue')
 
 
-label1 = tk.Label(root, text="Smart Attendance System", 
-font=("Courier New", 24), bg="blue", fg="white") 
+label1 = tk.Label(root, text="Smart Attendance System",
+font=("Courier New", 24), bg="blue", fg="white")
 label1.pack(padx=10,pady=10)
 
 date_label = tk.Label(root, text="Select Date:",font=("Courier New", 10), bg="skyblue")
 date_label.pack(pady=5)
-date_entry = tk.Entry(root) #adding entry field to enter values
-date_entry.pack(pady=5)
+
+#adding entry field to enter values
+date_entry = tk.Entry(root) 
+
 
 button1 = tk.Button(root, text="Select Excel File",width=20,height=2,
 bg="#B4A3D8",activebackground="grey",activeforeground="white")
