867a45f29598b956c96a1bcb984cc3bd45bc6d64
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-06-28 17:38:11 +0530

    Create 1.11 What is function in python.py

---
+++ 1.11 What is function in python.py
@@ -0,0 +1,17 @@
+# def is a keyword that we use to define the function.
+# addition is a function name here, a, b are a value or parameter.
+# Observe the indentation(spacing) of the code within the function on line no. 11.
+
+# Observe lines no. 13 and 14, same function called with different values.
+
+#Task 1: Uncomment lines 15 & 16 and fix the error.
+#Task 2: Call function subtraction with 156 and 100
+
+def addition(a, b):
+    print(a + b)
+
+addition(4, 6)  # Calls function with 4 and 6
+addition(10, 20)  # Calls function with 10 and 20
+
+#def subtraction(a, b):
+#print(a - b)
