655a97c668922f7dcce703287e123423601c2c7f
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-06-29 12:19:40 +0530

    Update 1.5(b):Mac: Enhancing button interactivity.py

--- 1.5(b):Mac: Enhancing button interactivity.py
+++ 1.5(b):Mac: Enhancing button interactivity.py
@@ -1,7 +1,7 @@
 #In this code, interactivity is added to the button using activebackground and activeforeground
 #options.
 #These settings change the button's appearance while it is being clicked by the user,
-#making the UI feel more responsive and dynamic
+#making the UI feel more responsive and dynam
 
 # Task: Change the button configuration when clicked to a different color.
 
@@ -13,13 +13,11 @@ root.geometry("500x300")
 
 root.config(bg='skyblue')
 
-
 label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), bg="blue", fg="white")
 label1.pack()
 
-#adding background and foreground color for the button when user clicks on it
+#adding background and foreground color for the button when the user clicks on it
 button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8",activebackground="grey",activeforeground="white")
-
 
 button1.config(highlightbackground = "black",highlightthickness=3)
 button1.pack()
