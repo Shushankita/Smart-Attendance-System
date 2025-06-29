43c52af8ee1d55cb20c168f9a647e0b682a0e72d
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-06-29 12:11:41 +0530

    Update 1.4(b): Mac: Customizing button widget in Tkinter: Width, height and Borders .py

--- 1.4(b): Mac: Customizing button widget in Tkinter: Width, height and Borders .py
+++ 1.4(b): Mac: Customizing button widget in Tkinter: Width, height and Borders .py
@@ -1,18 +1,34 @@
-#Task 1: change the value of highlightbackground from blue to black and observe the output.
+#bd (border width) defines how thick the border looks.
+#relief controls the style of the border (e.g., solid, flat, raised).
+#Together, these lines give the button a clear edge/border effect.
+
+#Task 1: change the bd value from 5 to 3 and observe the difference.
 
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Training Program")
 root.geometry("500x300")
-
 root.config(bg='skyblue')
 
-
-label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), bg="blue", fg="white") 
+label1 = tk.Label(
+    root,
+    text="Smart Attendance System",
+    font=("Courier New", 24),
+    bg="blue",
+    fg="white"
+)
 label1.pack()
 
-button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8") #defining the width and height of the button in terms of pixels
+button1 = tk.Button(
+    root,
+    text="Select Excel File",   # Text displayed on the button
+    width=20,                   # Width of the button (in character units)
+    height=2,                   # Height of the button (in text lines)
+    bg="#B4A3D8"                # Background color of the button
+)
+
+# Customize the button appearance
 button1.config(highlightbackground = "blue",highlightthickness=3)#adding border around the button and increasing it's thickness
 button1.pack()
 
