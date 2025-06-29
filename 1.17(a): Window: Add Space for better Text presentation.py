c16bad68bf9c0896d91e87785d3a6651d0f9c14a
Shushankita <107163460+Shushankita@users.noreply.github.com>
2025-06-29 12:58:28 +0530

    Update 1.17(a): Window: Add Space for better Text presentation.py

--- 1.17(a): Window: Add Space for better Text presentation.py
+++ 1.17(a): Window: Add Space for better Text presentation.py
@@ -35,12 +35,12 @@ date_var.trace('w', update)
 
 button1 = tk.Button(root, text="Select Excel File",width=20,height=2,
 bg="#B4A3D8",activebackground="grey",activeforeground="white")
-button1.config(highlightbackground = "black",highlightthickness=3)
+button1.config(bd = 5,relief="solid")
 button1.pack(pady=10)
 
 button2=tk.Button(root,text="Upload Photo",width=20,height=2,
 bg="#B4A3D8",activebackground="grey",activeforeground="white")
-button2.config(highlightbackground = "black",highlightthickness=3)
+button2.config(bd = 5,relief="solid")
 button2.pack(pady=10)
 
 text_widget = tk.Text(root,width=50,height=10,spacing1=10,spacing2=10,spacing3=10,state="disabled")
